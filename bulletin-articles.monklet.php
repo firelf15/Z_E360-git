b035bb5c8b6a160f12f530f2d9fc29d7f8f5fa1c
Monika Spielman <github@firelf.com>
2014-08-25 12:37:05 -0700

    Camp Celebration start
    The starting point for a monk let to display celebration articles.

---
+++ bulletin-articles.monklet.php
@@ -0,0 +1,19 @@
+{{
+   tag="article",
+   display="auto"
+   order="recent"
+   show_category="bulletin"
+   
+  before_show="<div class='items'>"
+  show="<article class='widget detail'>"
+  show="<header><ul class='media-icons'>
+                  </ul>"
+  show="<h3><a href='__url__'>__title__</a></h3></header>"
+show="<a href='__url__'><img src='__imageurl width='300'__' alt='' /></a>"
+  show="<p class='meta'><span class='times'>Posted: __date format='F j, Y'__</span>
+<span class='contact'>Contact: __authorX__ | __authorXemail__</span></p>"
+  show="<p class='details'>__text__</p>"
+  
+  show="</article><hr />"  
+  after_show="</div>"
+}}
---
+++ celebrate-articles-monklet.php
@@ -0,0 +1,19 @@
+{{
+   tag="article",
+   display="auto"
+   order="recent"
+   show_category="celebrate"
+   
+  before_show="<div class='items'>"
+  show="<article class='widget detail'>"
+  show="<header><ul class='media-icons'>
+                  </ul>"
+  show="<h3>__title__</h3></header>"
+show="<img src='__imageurl width='300'__' alt='' />"
+  show="<p class='meta'><span class='times'>Posted: __date format='F j, Y'__</span>
+<span class='contact'>Contact: __authorX__ | __authorXemail__</span></p>"
+  show="<p class='details'>__text__</p>"
+  
+  show="</article><hr />"  
+  after_show="</div>"
+}}
