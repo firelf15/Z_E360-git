c39cc797ffc10c34b09ce66fcdfb3946b424eebd
firelf15 <firelf@dewisant.local>
2014-11-18 12:01:50 -0800

    original code monklet link lists for staff page
    starting point of link list monklet

---
+++ staff-links-section-monklet.php
@@ -0,0 +1,15 @@
+{{
+  tag="linklist"
+  display="links"
+  before_show="<div class='widget featured'>"
+  show="<article class='pos-__position__'>"
+  show="<a"
+  show="__ifnewwindow__target='_blank'"
+  show=" href='__url__'"
+  show=">"
+  show="<img src='__imageurl width='300' height='159'__' alt='' />"
+  show="<span>__name__</span>"
+  show="</a>"
+  show="</article>"
+  after_show="</div>"
+}}
